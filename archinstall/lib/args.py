162efbecf6144b6bd97c303cf5fab49a4abe41f5
IUenkodr <jmlanticse247@gmail.com>
2026-09-14 19:52:58 +0000

    Fix mangled match block in args.py for the final time

--- archinstall/lib/args.py
+++ archinstall/lib/args.py
@@ -136,11 +136,9 @@ class ArchConfigType(StrEnum):
 			case ArchConfigType.PACKAGES:
 				return tr('Additional packages')
 			case ArchConfigType.PACMAN_CONFIG:
-			case ArchConfigType.PACMAN_CONFIG:
 				return tr('Pacman')
 			case ArchConfigType.THIRD_PARTY_REPOS:
 				return tr("Third Party Repositories")
-				return tr('Pacman')
 			case ArchConfigType.CUSTOM_COMMANDS:
 				return tr('Custom commands')
 			case ArchConfigType.USERS:
